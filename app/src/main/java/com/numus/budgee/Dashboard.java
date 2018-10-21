7a0257cbe9619267b211c81a852ad9ffc81e3419
jekotasek <josekotasek@gmail.com>
2018-10-21 00:43:00 -0500

    Updated navbar

--- app/src/main/java/com/numus/budgee/Dashboard.java
+++ app/src/main/java/com/numus/budgee/Dashboard.java
@@ -4,32 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class Dashboard extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_expenses);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_summary);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_profile);
-                    return true;
-            }
-            return false;
-        }
-    };
+    TextView mTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,5 +21,32 @@ public class Dashboard extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_expenses:
+                    mTextMessage.setText(R.string.title_expenses);
+                    return true;
+                case R.id.navigation_summary:
+                    Log.i("Salu2",R.string.title_summary+"");
+                    Log.i("Salu2",item.getItemId()+"");
+                    Log.i("Salu2",R.id.navigation_summary+"");
+                    Log.i("Salu2",mTextMessage.getId()+"");
+                    mTextMessage.setText(R.string.title_summary);
+                    return true;
+                case R.id.navigation_profile:
+                    mTextMessage.setText(R.string.title_profile);
+                    return true;
+                case R.id.navigation_settings:
+                    mTextMessage.setText(R.string.title_settings);
+                    return true;
+            }
+            return false;
+        }
+    };
 
 }
