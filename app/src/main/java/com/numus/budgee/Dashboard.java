672bf7a0a439a8f6fdb3ee8ce727b7f4d2ec39e4
jekotasek <josekotasek@gmail.com>
2018-10-21 01:40:16 -0500

    Connected fragments to nav bar

--- app/src/main/java/com/numus/budgee/Dashboard.java
+++ app/src/main/java/com/numus/budgee/Dashboard.java
@@ -3,6 +3,7 @@ package com.numus.budgee;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
@@ -10,16 +11,21 @@ import android.widget.TextView;
 
 public class Dashboard extends AppCompatActivity {
 
-    TextView mTextMessage;
+    //TextView mTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        //mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        ExpensesFragment fragmentA = new ExpensesFragment();
+        FragmentTransaction fragTransA = getSupportFragmentManager().beginTransaction();
+        fragTransA.replace(R.id.frame,fragmentA,"ExpensesFragment");
+        fragTransA.commit();
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -29,20 +35,32 @@ public class Dashboard extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_expenses:
-                    mTextMessage.setText(R.string.title_expenses);
+                    //mTextMessage.setText(R.string.title_expenses);
+                    ExpensesFragment fragmentA = new ExpensesFragment();
+                    FragmentTransaction fragTransA = getSupportFragmentManager().beginTransaction();
+                    fragTransA.replace(R.id.frame,fragmentA,"ExpensesFragment");
+                    fragTransA.commit();
                     return true;
                 case R.id.navigation_summary:
-                    Log.i("Salu2",R.string.title_summary+"");
-                    Log.i("Salu2",item.getItemId()+"");
-                    Log.i("Salu2",R.id.navigation_summary+"");
-                    Log.i("Salu2",mTextMessage.getId()+"");
-                    mTextMessage.setText(R.string.title_summary);
+                    //mTextMessage.setText(R.string.title_summary);
+                    SummaryFragment fragmentB = new SummaryFragment();
+                    FragmentTransaction fragTransB = getSupportFragmentManager().beginTransaction();
+                    fragTransB.replace(R.id.frame,fragmentB,"SummaryFragment");
+                    fragTransB.commit();
                     return true;
                 case R.id.navigation_profile:
-                    mTextMessage.setText(R.string.title_profile);
+                    //mTextMessage.setText(R.string.title_profile);
+                    ProfileFragment fragmentC = new ProfileFragment();
+                    FragmentTransaction fragTransC = getSupportFragmentManager().beginTransaction();
+                    fragTransC.replace(R.id.frame,fragmentC,"ProfileFragment");
+                    fragTransC.commit();
                     return true;
                 case R.id.navigation_settings:
-                    mTextMessage.setText(R.string.title_settings);
+                    //mTextMessage.setText(R.string.title_settings);
+                    SettingsFragment fragmentD = new SettingsFragment();
+                    FragmentTransaction fragTransD = getSupportFragmentManager().beginTransaction();
+                    fragTransD.replace(R.id.frame,fragmentD,"SettingsFragment");
+                    fragTransD.commit();
                     return true;
             }
             return false;
